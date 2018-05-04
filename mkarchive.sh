a2df5480fbb4a2249a8bc15b81f216b58d7a239b
Shaun Guth <shaun.guth@gmail.com>
2018-05-04 18:22:00 +0000

    quick and dirty script

---
+++ mkarchive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf archive
+number=0 git rev-list master | while read rev; do
+  fmt_num=$(printf -- '%03d' $number)
+  mkdir -p archive/$fmt_num
+  git archive $rev | (cd archive/$fmt_num; tar x)
+  (( ++number ))
+  sed -e '5i<meta http-equiv="refresh" content="2;url=../'$(printf -- '%03d' $number)'/index.html"/>' -i archive/$fmt_num/index.html
+done
